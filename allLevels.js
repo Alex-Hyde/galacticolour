a5e5350d174e143488cfb5915b2ce15398f25d3c
Sebvillate <sebastian_villate@hotmail.com>
2021-01-02 19:16:35 -0500

    committing

--- allLevels.js
+++ allLevels.js
@@ -1,5 +1,5 @@
 function createLevelList() {
-    level1 = new Level([new Wave([[Mothership, 5], [PurpleSinMob, 7], [Mob3, 10]], 22, 1500), new Wave([[YellowSinMob, 3], [PurpleSinMob, 3], [Mob3, 6]], 12, 1500)], 1);
+    level1 = new Level([new Wave([[Mothership, 1], [purpleTank, 1], [redMedic, 1]], 1500), new Wave([[YellowSinMob, 3], [PurpleSinMob, 3], [Mob3, 6]], 12, 1500)], 1);
     level2 = new Level([new Wave([[PurpleSinMob, 7], [Mob3, 10]], 17, 1500), new Wave([[YellowSinMob, 3], [PurpleSinMob, 3]], 6, 1500)], 2);
     level3 = new Level([new Wave([[yellowLeftTank, 3], [redTracker, 9], [PurpleSinMob, 0], [Mob3, 0]], 12, 1500), new Wave([[YellowSinMob, 5], [PurpleSinMob, 2], [Mob3, 6]], 13, 1500)], 3);
     level4 = new levelScroll1(4);
